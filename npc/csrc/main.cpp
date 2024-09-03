6c3ec1a0af60c9fd5a04b887b3fe94ba4ee0c706
heyuanhong <497654878@qq.com>
2024-09-03 20:47:50 +0800

    before starting pa1

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,31 @@
-#include <stdio.h>
+#include <Vlight.h>
+#include <nvboard.h>
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+
+static TOP_NAME top;
+
+void nvboard_bind_all_pins(TOP_NAME* top);
+
+static void single_cycle() {
+  top.clk = 0; top.eval();
+  top.clk = 1; top.eval();
+}
+
+static void reset(int n) {
+  top.rst = 1;
+  while (n -- > 0) single_cycle();
+  top.rst = 0;
+}
 
 int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+  nvboard_bind_all_pins(&top);
+  nvboard_init();
+
+  reset(10);
+
+  while(1) {
+    nvboard_update();
+    single_cycle();
+  }
 }
