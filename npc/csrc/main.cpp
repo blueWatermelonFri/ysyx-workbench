1b8cc0182823dde3db4a9d0c64c3dddeb48d5739
tracer-ysyx <tracer@ysyx.org>
2024-09-03 20:18:35 +0800

    > sim RTL ysyx_22040000 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:18:35 up 17 days, 22:32,  0 user,  load average: 2.87, 3.55, 4.10

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <Vtop.h>
+#include <nvboard.h>
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+
+static TOP_NAME top;
 
 int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+
+  while (1) {
+    int a = rand() & 1;
+    int b = rand() & 1;
+    top.a = a;
+    top.b = b;
+    top.eval();
+    printf("a = %d, b = %d, f = %d\n", a, b, top.f);
+    assert(top.f == (a ^ b));
+  }
 }
