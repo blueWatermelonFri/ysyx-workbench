670aca3ad381b9592c3e04900b775f94109ee198
tracer-ysyx <tracer@ysyx.org>
2024-09-05 22:18:11 +0800

    > compile NEMU ysyx_22040000 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:18:11 up  2:25,  0 user,  load average: 0.21, 0.18, 0.12

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -43,9 +43,6 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 static void exec_once(Decode *s, vaddr_t pc) {
   s->pc = pc;
   s->snpc = pc;
-  printf("%x\n", s->pc);
-  printf("%x\n", s->pc);
-  printf("%x\n", s->pc);
   isa_exec_once(s);
   cpu.pc = s->dnpc;
 #ifdef CONFIG_ITRACE
