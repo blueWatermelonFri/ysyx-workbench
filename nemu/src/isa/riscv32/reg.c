c0238bccc0c055748782debb4ff3415cd397a76f
tracer-ysyx <tracer@ysyx.org>
2024-09-07 15:58:22 +0800

    > compile NEMU ysyx_22040000 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:58:22 up 1 day, 20:05,  0 user,  load average: 4.48, 3.43, 3.10

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -23,7 +23,7 @@ const char *regs[] = {
   "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
 };
 
-void isa_reg_display() {
+void isa_reg_display(char *args) {
    printf("reg0 = 0x%08x\n", cpu.gpr[0]);
    printf("reg1 = 0x%08x\n", cpu.gpr[1]);
    printf("reg2 = 0x%08x\n", cpu.gpr[2]);
@@ -56,8 +56,86 @@ void isa_reg_display() {
   printf("reg29 = 0x%08x\n", cpu.gpr[29]);
   printf("reg30 = 0x%08x\n", cpu.gpr[30]);
   printf("reg31 = 0x%08x\n", cpu.gpr[31]);
+
+    bool *success = NULL;  // 初始化为 NULL
+  *success = true;
+  char *str_reg = strtok(args," ");
+  int reg_num = isa_reg_str2val(str_reg, success);
+  printf("reg%d = 0x%08x\n", reg_num, cpu.gpr[reg_num]);
+
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
+
+    *success = true;
+    if (strcmp(s, "$0") == 0) {
+        return 0;
+    } else if (strcmp(s, "ra") == 0) {
+        return 1;
+    } else if (strcmp(s, "sp") == 0) {
+        return 2;
+    } else if (strcmp(s, "gp") == 0) {
+        return 3;
+    } else if (strcmp(s, "tp") == 0) {
+        return 4;
+    } else if (strcmp(s, "t0") == 0) {
+        return 5;
+    } else if (strcmp(s, "t1") == 0) {
+        return 6;
+    } else if (strcmp(s, "t2") == 0) {
+        return 7;
+    } else if (strcmp(s, "s0") == 0) {
+        return 8;
+    } else if (strcmp(s, "s1") == 0) {
+        return 9;
+    } else if (strcmp(s, "a0") == 0) {
+        return 10;
+    } else if (strcmp(s, "a1") == 0) {
+        return 11;
+    } else if (strcmp(s, "a2") == 0) {
+        return 12;
+    } else if (strcmp(s, "a3") == 0) {
+        return 13;
+    } else if (strcmp(s, "a4") == 0) {
+        return 14;
+    } else if (strcmp(s, "a5") == 0) {
+        return 15;
+    } else if (strcmp(s, "a6") == 0) {
+        return 16;
+    } else if (strcmp(s, "a7") == 0) {
+        return 17;
+    } else if (strcmp(s, "s2") == 0) {
+        return 18;
+    } else if (strcmp(s, "s3") == 0) {
+        return 19;
+    } else if (strcmp(s, "s4") == 0) {
+        return 20;
+    } else if (strcmp(s, "s5") == 0) {
+        return 21;
+    } else if (strcmp(s, "s6") == 0) {
+        return 22;
+    } else if (strcmp(s, "s7") == 0) {
+        return 23;
+    } else if (strcmp(s, "s8") == 0) {
+        return 24;
+    } else if (strcmp(s, "s9") == 0) {
+        return 25;
+    } else if (strcmp(s, "s10") == 0) {
+        return 26;
+    } else if (strcmp(s, "s11") == 0) {
+        return 27;
+    } else if (strcmp(s, "t3") == 0) {
+        return 28;
+    } else if (strcmp(s, "t4") == 0) {
+        return 29;
+    } else if (strcmp(s, "t5") == 0) {
+        return 30;
+    } else if (strcmp(s, "t6") == 0) {
+        return 31;
+    } else {
+        success = false;
+        printf("The '%s' is not in registers list\n", s);
+    }
+
   return 0;
 }
