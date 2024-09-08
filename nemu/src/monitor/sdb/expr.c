28c29dee4498089347dcbc90df81ff6b7002316a
tracer-ysyx <tracer@ysyx.org>
2024-09-08 17:45:44 +0800

    > compile NEMU ysyx_22040000 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:45:44 up 2 days, 21:52,  0 user,  load average: 0.27, 0.23, 0.16

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -312,7 +312,7 @@ word_t expr(char *e, bool *success)
     }
 
     /* TODO: Insert codes to evaluate the expression. */
-
+    printf("%d\n", nr_token-1);
     printf("%d\n", eval(0, nr_token - 1));
 
     return 0;
