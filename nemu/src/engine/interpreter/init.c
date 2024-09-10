9f8789f484b906d4c4f1e5afad449abb86513aa3
tracer-ysyx <tracer@ysyx.org>
2024-09-10 21:00:42 +0800

    > compile NEMU ysyx_22040000 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:00:42 up 5 days,  1:07,  0 user,  load average: 1.31, 1.37, 1.50

--- nemu/src/engine/interpreter/init.c
+++ nemu/src/engine/interpreter/init.c
@@ -14,15 +14,54 @@
 ***************************************************************************************/
 
 #include <cpu/cpu.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE 65536 + 1024
 
 void sdb_mainloop();
 extern int cmd_p(char *args);
 
+void test_cmd_p(){
+    FILE *file = fopen("/home/myuser/ysyx/ysyx-workbench/nemu/tools/gen-expr/input", "r"); // 打开文件
+    if (!file) {
+        perror("无法打开文件");
+        return;
+    }
+
+    char line[MAX_LINE];   // 存储每一行
+    char A[10] = ""; // 存储第一个数字
+    char B[MAX_LINE] = ""; // 存储其余内容
+
+    while (fgets(line, sizeof(line), file)) { // 读取每一行
+      char tempA[MAX_LINE], tempB[MAX_LINE];
+    
+    // 使用 sscanf 读取第一个数字
+    if (sscanf(line, "%s %[^\n]", tempA, tempB) == 2) {
+        strcat(A, tempA);
+        strcat(A, " "); // 保持数字之间的空格
+        strcat(B, tempB);
+        strcat(B, "\n"); // 保持原行的换行符
+    }
+    }
+    
+    printf("字符串 A: \n%s\n", A);
+    printf("字符串 B: \n%s\n", B);
+
+    fclose(file);
+    return;
+}
+
 void engine_start() {
 #ifdef CONFIG_TARGET_AM
-  cpu_exec(-1);
+  cpu_exec(-1); 
 #else
   /* Receive commands from user. */
-  sdb_mainloop();
+  #if 1
+    sdb_mainloop();
+  #else
+    test_cmd_p();
+  #endif
 #endif
 }
