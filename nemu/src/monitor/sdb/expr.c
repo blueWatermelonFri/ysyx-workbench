580e648b7ab133a7750ac36514d1cebe40e4932d
tracer-ysyx <tracer@ysyx.org>
2024-09-12 22:30:37 +0800

    > compile NEMU ysyx_22040000 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:30:37 up 12:02,  0 user,  load average: 1.84, 1.87, 1.82

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -196,7 +196,8 @@ static bool make_token(char *e)
                     break; 
                 case REG:
                     tokens[nr_token].type = REG;
-                    strncpy(tokens[nr_token].str, substr_start, substr_len);
+                    // 不保存$符号
+                    strncpy(tokens[nr_token].str, substr_start+1, substr_len);
                     tokens[nr_token].str[substr_len] = '\0';
                     break;                                            
                 default:
