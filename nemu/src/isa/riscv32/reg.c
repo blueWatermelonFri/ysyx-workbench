d72457e9c9683d50de518914ff47ce19c4dae9c2
tracer-ysyx <tracer@ysyx.org>
2024-09-12 22:21:15 +0800

    > compile NEMU ysyx_22040000 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:21:15 up 11:52,  0 user,  load average: 1.72, 1.87, 1.75

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -59,11 +59,10 @@ void isa_reg_display(char *args) {
 
   printf("11111111111111\n");
 //   bool *success = NULL;  // 初始化为 NULL
-  bool *success = false;
-  *success = true;
+  bool success = false;
   printf("11111111111111\n");
   char *str_reg = strtok(args," ");
-  int reg_num = isa_reg_str2val(str_reg, success);
+  int reg_num = isa_reg_str2val(str_reg, &success);
   printf("reg%d = 0x%08x\n", reg_num, cpu.gpr[reg_num]);
 
 }
