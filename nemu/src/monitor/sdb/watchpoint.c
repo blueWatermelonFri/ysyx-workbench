13819918b9e9d2976d3650406566047faa8c7b03
tracer-ysyx <tracer@ysyx.org>
2024-09-27 22:28:11 +0800

    > compile NEMU ysyx_22040000 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:28:11 up 7 days, 12:45,  0 user,  load average: 1.52, 1.39, 1.38

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -144,6 +144,7 @@ void difftest_wp(){
     while(cur != NULL){
         success = false;
         res = expr(cur->expression, &success);
+        printf("%s\n", cur->expression);
         assert(success);
         if(res != cur->cur_value){
           if(flag == 0){
