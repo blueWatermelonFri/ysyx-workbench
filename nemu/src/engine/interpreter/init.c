8577fbd1345522d614d7013080dce2bab61c00b5
heyuanhong <497654878@qq.com>
2024-09-29 20:15:03 +0800

    update expr test error

--- nemu/src/engine/interpreter/init.c
+++ nemu/src/engine/interpreter/init.c
@@ -39,10 +39,13 @@ void test_cmd_p(){
     
       //  使用 sscanf 读取第一个数字
       if (sscanf(line, "%s %[^\n]", A, B) == 2) {
+          bool *suceess = NULL;
+          suceess =  (bool *)malloc(sizeof(bool)*1);
+          *suceess = false;
 
           char * endptr ;
           word_t real = (word_t) strtoul(A, &endptr, 0);
-          word_t res = expr(B, false);
+          word_t res = expr(B, suceess);
 
           if(real != res){
             fprintf(stderr, "real is %u, res is %u\n", real, res);
@@ -58,7 +61,7 @@ void engine_start() {
   cpu_exec(-1); 
 #else
   /* Receive commands from user. */
-  #if 1
+  #if 0
     sdb_mainloop();
   #else
     // test expr compute 
