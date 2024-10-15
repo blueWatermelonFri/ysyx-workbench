9da698fe15aa80486d68432af7686816f62c40cb
tracer-ysyx <tracer@ysyx.org>
2024-10-15 21:16:09 +0800

    > compile NEMU ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:16:09 up 25 days, 11:33,  0 user,  load average: 3.25, 2.49, 2.36

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -75,6 +75,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   p[0] = '\0'; // the upstream llvm does not support loongarch32r
 #endif
 #endif
+  printf("exex_over\n");
 }
 
 static void execute(uint64_t n) {
