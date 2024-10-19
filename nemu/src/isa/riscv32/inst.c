cd66c029248567df78204898c8e7667a83b36f0e
tracer-ysyx <tracer@ysyx.org>
2024-10-19 17:54:56 +0800

    > compile NEMU ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:54:56 up 29 days,  8:12,  0 user,  load average: 0.83, 0.84, 0.78

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -28,15 +28,15 @@ enum {
   TYPE_N, // none
 };
 
-// I U S J rok
+// I U S J r B ok
 // SEXT(BITS(i, 31, 12), 20) << 12，用到了C语言复合语句相关的东西
 #define src1R() do { *src1 = R(rs1); } while (0)
 #define src2R() do { *src2 = R(rs2); } while (0)
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
-#define immB() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
-#define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
+#define immB() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 12) | (USEXT(BITS(i, 8, 7), 1) << 11) | (USEXT(BITS(i, 30, 25), 6) << 5) | (USEXT(BITS(i, 11, 8), 4) << 1); } while(0)
 #define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | (USEXT(BITS(i, 19, 12), 8) << 12) | (USEXT(BITS(i, 20, 20), 1) << 11) | (USEXT(BITS(i, 30, 21), 10) << 1); } while(0)
+#define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define USimmU() do { *imm = USEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
@@ -79,6 +79,9 @@ static int decode_exec(Decode *s) {
   INSTPAT("0100000 ????? ????? 000 ????? 01100 11", sub    , r, R(rd) = src1 - src2);
 
   INSTPAT("??????? ????? ????? 011 ????? 00100 11", sltiu  , USI, R(rd) = (src1 < imm) ? 1 : 0);
+
+  INSTPAT("??????? ????? ????? 000 ????? 11000 11", beq    , B, if(src1 == src2) {s->dnpc = s->pc + imm;});
+
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
