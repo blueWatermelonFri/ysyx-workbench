b14466c2be2da3ca5cdc7f6f672d5907b114fb4c
tracer-ysyx <tracer@ysyx.org>
2024-10-23 21:58:45 +0800

    > compile NEMU ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:58:45 up 33 days, 12:16,  0 user,  load average: 0.58, 0.64, 0.67

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -121,7 +121,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   
   // RV32M
-  INSTPAT("0000001 ????? ????? 000 ????? 01100 11", mul    , R, R(rd) = src1 * src2);
+  INSTPAT("0000001 ????? ????? 000 ????? 01100 11", mul    , R, R(rd) = (int)src1 * (int)src2);
   INSTPAT("0000001 ????? ????? 001 ????? 01100 11", mulh   , R, uint64_t tmp = (uint64_t)src1 * (uint64_t)src2; R(rd) = (uint32_t)(((uint64_t) tmp) >> 32));
 
   INSTPAT("0000001 ????? ????? 100 ????? 01100 11", Div    , R, R(rd) = (int)src1 / (int)src2);
