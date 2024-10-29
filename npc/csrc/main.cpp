a5998307e4df00cf40b5a13feddc0635ccd25268
tracer-ysyx <tracer@ysyx.org>
2024-10-29 20:33:39 +0800

    > sim RTL ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:33:39 up 39 days, 10:51,  0 user,  load average: 1.61, 1.60, 1.61

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,11 +1,34 @@
-#include <Vlight.h>
-#include <nvboard.h>
+#include <Vysyx_24100005_top.h>
+// #include <nvboard.h>
 #include "verilated.h"
 #include "verilated_vcd_c.h"
 
+#define PG_ALIGN __attribute((aligned(4096)))
 static TOP_NAME top;
 
-void nvboard_bind_all_pins(TOP_NAME* top);
+static const uint32_t img [] = {
+  0x1c00000c,  // pcaddu12i $t0,0
+  0x29804180,  // st.w $zero,$t0,16
+  0x28804184,  // ld.w $a0,$t0,16
+  0x002a0000,  // break 0 (used as nemu_trap)
+  0xdeadbeef,  // some data
+};
+
+
+static uint8_t pmem[4*10] PG_ALIGN = {};
+
+uint8_t* guest_to_host(uint32_t paddr) { return pmem + paddr - 0x80000000; }
+
+static inline uint32_t host_read(void *addr) {
+    return *(uint32_t *)addr;
+}
+
+static uint32_t pmem_read(uint32_t addr) {
+  uint32_t ret = host_read(guest_to_host(addr));
+  return ret;
+}
+
+// void nvboard_bind_all_pins(TOP_NAME* top);
 
 static void single_cycle() {
   top.clk = 0; top.eval();
@@ -19,13 +42,16 @@ static void reset(int n) {
 }
 
 int main() {
-  nvboard_bind_all_pins(&top);
-  nvboard_init();
+  // nvboard_bind_all_pins(&top);
+  // nvboard_init();
+  memcpy(guest_to_host(0x80000000), img, sizeof(img));
 
   reset(10);
 
-  while(1) {
-    nvboard_update();
-    single_cycle();
+  int tmp = 4;
+  while (tmp) {
+    top.inst = pmem_read(top.PC);
+    top.eval();
+    tmp --;
   }
 }
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,22 +1,24 @@
-module top(
-  input a,
-  input b,
-  output f
-);
-  assign f = a ^ b;
-endmodule
-
-module light(
-  input clk,
+module ysyx_24100005_top(
+  input [31:0] inst,
   input rst,
-  output reg [15:0] led
+  input clk,
+
+  output reg [31:0] PC
 );
-  reg [31:0] count;
+  wire [31:0] Next_PC;
+
+  ysyx_24100005_Reg #(32, 32'h8000_0000) i0 (.clk(clk), 
+                                              .rst(rst), 
+                                              .din(Next_PC), 
+                                              .dout(PC), 
+                                              .wen(1'b1));
+  
+
+  assign Next_PC = PC + 32'h0000_0004;
+
+
   always @(posedge clk) begin
-    if (rst) begin led <= 1; count <= 0; end
-    else begin
-      if (count == 0) led <= {led[14:0], led[15]};
-      count <= (count >= 5000000 ? 32'b0 : count + 1);
-    end
+    $display("inst=%h, ", inst);
   end
+
 endmodule
