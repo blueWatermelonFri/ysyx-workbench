df37ede76f5e115b34fe9024c183d67014450c1e
tracer-ysyx <tracer@ysyx.org>
2024-10-29 21:36:09 +0800

    > sim RTL ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:36:09 up 39 days, 11:53,  0 user,  load average: 1.27, 1.47, 1.56

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -7,6 +7,11 @@ module ysyx_24100005_top(
 );
   wire [31:0] Next_PC;
   wire [6:0] Opcode;
+  wire [2:0] funct3;
+  wire [4:0] rs1;
+  wire [11:0] imm;
+  wire [4:0] rd;
+
   ysyx_24100005_Reg #(32, 32'h8000_0000) i0 (.clk(clk), 
                                               .rst(rst), 
                                               .din(Next_PC), 
@@ -14,6 +19,12 @@ module ysyx_24100005_top(
                                               .wen(1'b1));
   assign Next_PC = PC + 32'h0000_0004;
   assign Opcode = inst[6:0];
+  assign funct3 = inst[14:12];
+  assign rs1 = inst[19:15];
+  assign imm = inst[31:20];
+  assign rd = inst[11:7];
+
+
 
 
 
