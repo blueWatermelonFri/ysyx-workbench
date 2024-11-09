350bd30bbdd272a673953b76479503a748c85ee5
tracer-ysyx <tracer@ysyx.org>
2024-11-09 22:34:18 +0800

    > compile NEMU ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:34:18 up 50 days, 12:51,  0 user,  load average: 1.13, 1.39, 1.48

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -55,8 +55,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
       //s->dnpc表示跳转的下一条指令
       if(_this->isa.inst.val == 0x00008067){ 
           for(int i = 0 ; i < func_count; i++){
-            if(cpu.gpr[1] >= func_begin[i] && cpu.gpr[1] >= func_end[i]){
-                printf("%x\n", cpu.gpr[1]);
+            if(_this->pc >= func_begin[i] && _this->pc >= func_end[i]){
                 printf("%*sret %s\n", ftrace_cnt, "", func_name[i]);
                 ftrace_cnt --;
                 break;
