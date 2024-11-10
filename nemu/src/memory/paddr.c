eb3ec81c8bfb645865c2c91187f0c5e22392ec15
tracer-ysyx <tracer@ysyx.org>
2024-11-10 13:50:25 +0800

    > compile NEMU ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:50:25 up 51 days,  4:08,  0 user,  load average: 1.68, 1.68, 1.65

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -51,22 +51,24 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  if (likely(in_pmem(addr))) return pmem_read(addr, len);
-  IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
-  printf("read address = " FMT_PADDR, addr);
 
 #ifdef CONFIG_MTRACE
   printf("read address = " FMT_PADDR, addr);
 #endif
+
+  if (likely(in_pmem(addr))) return pmem_read(addr, len);
+  IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
   return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
-  IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
+
 #ifdef CONFIG_MTRACE
   printf("write address = " FMT_PADDR, addr);
 #endif
+
+  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
+  IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }
