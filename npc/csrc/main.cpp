0582bc3854593933723e8645131775d34ab13a27
tracer-ysyx <tracer@ysyx.org>
2024-11-17 17:46:56 +0800

    > sim RTL ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:46:56 up 58 days,  8:04,  0 user,  load average: 1.43, 1.55, 1.57

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,71 +1,12 @@
-#include <Vysyx_24100005_top.h>
 // #include <nvboard.h>
-#include "verilated.h"
-#include "verilated_vcd_c.h"
+#include "npc_common.h"
+#include "sdb.h"
+#include "tool.h"
+
 // #include <stdio.>
 
-#define PG_ALIGN __attribute((aligned(4096)))
-#define RESET_VECTOR 0x80000000
-static TOP_NAME top;
-
-// dummy 
-static const uint32_t img [] = {
-	0x00000413,
-	0x00009117,
-	0xffc10113,
-	0x00c000ef,
-	0x00000513,
-	0x00008067,
-	0xff410113,
-	0x00000517,
-	0x01c50513,
-	0x00112423,
-	0xfe9ff0ef,
-	0x00050513,
-	0x00100073,
-	0x0000006f
-};
 
 
-
-static uint8_t pmem[4*1000] PG_ALIGN = {};
-
-uint8_t* guest_to_host(uint32_t paddr) { return pmem + paddr - 0x80000000; }
-
-static inline uint32_t host_read(void *addr) {
-    return *(uint32_t *)addr;
-}
-
-static uint32_t pmem_read(uint32_t addr) {
-  printf("addr = %x\n", addr);
-  uint32_t ret = host_read(guest_to_host(addr));
-  return ret;
-}
-
-static char *img_file = NULL;
-static long load_img() {
-  if (img_file == NULL) {
-    printf("No image is given. Use the default build-in image.");
-    return 4096; // built-in image size
-  }
-
-  FILE *fp = fopen(img_file, "rb");
-  if(!fp){
-    printf("Can not open '%s'", img_file);
-  }
-
-  fseek(fp, 0, SEEK_END);
-  long size = ftell(fp);
-
-  printf("The image is %s, size = %ld\n", img_file, size);
-
-  fseek(fp, 0, SEEK_SET);
-  int ret = fread(guest_to_host(RESET_VECTOR), size, 1, fp);
-  assert(ret == 1);
-
-  fclose(fp);
-  return size;
-}
 
 extern "C" void ebreak() {
   printf("hit at goog trap\n");
@@ -74,30 +15,18 @@ extern "C" void ebreak() {
 
 // void nvboard_bind_all_pins(TOP_NAME* top);
 
-static void single_cycle() {
-  top.clk = 0; top.eval();
-  top.clk = 1; top.eval();
-}
-
-static void reset(int n) {
-  top.rst = 1;
-  while (n -- > 0) single_cycle();
-  top.rst = 0;
-}
 
 int main(int argc, char *argv[]) {
   // nvboard_bind_all_pins(&top);
   // nvboard_init();
-  memcpy(guest_to_host(0x80000000), img, sizeof(img));
-  img_file = argv[1];
-  load_img();
+  init_img(argv[1]);
 
   reset(3);
 
-  int tmp = 10;
-  while (1) {
-    top.inst = pmem_read(top.PC);
-    single_cycle();
-    tmp --;
-  }
+  npc_sdb_mainloop();
+
+  // while (1) {
+  //   top.inst = pmem_read(top.PC);
+  //   single_cycle();
+  // }
 }
---
+++ npc/csrc/tool.cpp
@@ -0,0 +1,85 @@
+#include "npc_common.h"
+
+#define PG_ALIGN __attribute((aligned(4096)))
+#define RESET_VECTOR 0x80000000
+
+static TOP_NAME top;
+// dummy 
+static const uint32_t img [] = {
+	0x00000413,
+	0x00009117,
+	0xffc10113,
+	0x00c000ef,
+	0x00000513,
+	0x00008067,
+	0xff410113,
+	0x00000517,
+	0x01c50513,
+	0x00112423,
+	0xfe9ff0ef,
+	0x00050513,
+	0x00100073,
+	0x0000006f
+};
+
+static uint8_t pmem[4*1000] PG_ALIGN = {};
+
+uint8_t* guest_to_host(uint32_t paddr) { return pmem + paddr - 0x80000000; }
+
+static inline uint32_t host_read(void *addr) {
+    return *(uint32_t *)addr;
+}
+
+static uint32_t pmem_read(uint32_t addr) {
+  printf("addr = %x\n", addr);
+  uint32_t ret = host_read(guest_to_host(addr));
+  return ret;
+}
+
+
+
+void single_cycle() {
+  top.clk = 0; top.eval();
+  top.clk = 1; top.eval();
+}
+
+void reset(int n) {
+  top.rst = 1;
+  while (n -- > 0) single_cycle();
+  top.rst = 0;
+}
+
+void npc_execute_once(){
+    top.inst = pmem_read(top.PC);
+    single_cycle();
+}
+
+static long load_img(char* img_file) {
+  if (img_file == NULL) {
+    printf("No image is given. Use the default build-in image.");
+    return 4096; // built-in image size
+  }
+
+  FILE *fp = fopen(img_file, "rb");
+  if(!fp){
+    printf("Can not open '%s'", img_file);
+  }
+
+  fseek(fp, 0, SEEK_END);
+  long size = ftell(fp);
+
+  printf("The image is %s, size = %ld\n", img_file, size);
+
+  fseek(fp, 0, SEEK_SET);
+  int ret = fread(guest_to_host(RESET_VECTOR), size, 1, fp);
+  assert(ret == 1);
+
+  fclose(fp);
+  return size;
+}
+
+void init_img(char* img_file){
+
+  memcpy(guest_to_host(0x80000000), img, sizeof(img));
+  load_img(img_file);
+}
