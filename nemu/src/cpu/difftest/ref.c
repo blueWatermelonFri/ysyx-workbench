26f97a4e6e3c783921790c835e44dcbe30855d8b
tracer-ysyx <tracer@ysyx.org>
2024-11-22 22:31:49 +0800

    > compile NEMU ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:31:49 up 63 days, 12:49,  0 user,  load average: 1.45, 1.60, 1.62

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -20,7 +20,6 @@
 
 void diff_set_regs(void *dut){
   CPU_state * ctx = ( CPU_state *)dut;
-  printf("ctx-pc = %x\n", ctx->pc);
 
   for (int i = 0;  i< 32; i ++){
     cpu.gpr[i] = ctx->gpr[i];
@@ -47,8 +46,6 @@ __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
 }
 
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
-     CPU_state * ctx = (CPU_state *)dut;
-      printf("ctx-pc = %x\n", ctx->pc);
 
   if (direction == DIFFTEST_TO_REF) {
     diff_set_regs(dut);
