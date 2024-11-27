19fa4743ebe3fe4feece4b37389d70e1b3c7f48e
tracer-ysyx <tracer@ysyx.org>
2024-11-27 19:34:03 +0800

    > sim RTL ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:34:03 up 68 days,  9:51,  0 user,  load average: 1.42, 1.52, 1.64

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -296,7 +296,6 @@ module ysyx_24100005_top(
       $display("rs2data   =%h", rs2data);
       $display("wmask     =%h", wmask);
 
-
     if (read_mem) begin // 有读写请求时 // 可以进一步优化吗，因为代码的逻辑是要写的话就必须读
       mem_rdata = npcmem_read(add_output);
       if (write_mem) begin // 有写请求时
