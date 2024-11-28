911b1c5f188bb8891edeea8410389621bda3d64b
tracer-ysyx <tracer@ysyx.org>
2024-11-28 22:22:36 +0800

    > sim RTL ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:22:36 up 69 days, 12:40,  0 user,  load average: 1.08, 0.80, 0.72

--- npc/csrc/tool.cpp
+++ npc/csrc/tool.cpp
@@ -132,13 +132,13 @@ extern "C" void npcmem_write(int waddr, int wdata, char wmask) {
 void single_cycle() {
 
   contextp->timeInc(1);
-  printf("clk = 1\n");
-  top.clk = 1; top.eval();
+  printf("clk = 0\n");
+  top.clk = 0; top.eval();
   tfp->dump(contextp->time());
 
   contextp->timeInc(1);
-  printf("clk = 0\n");
-  top.clk = 0; top.eval();
+  printf("clk = 1\n");
+  top.clk = 1; top.eval();
   tfp->dump(contextp->time());
 
 }
