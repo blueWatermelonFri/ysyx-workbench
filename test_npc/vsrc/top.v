c2b69f572ec41034ae9a66b9134f305628aa280f
tracer-ysyx <tracer@ysyx.org>
2024-11-28 10:28:58 +0800

    > sim RTL ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:28:58 up 69 days, 46 min,  0 user,  load average: 2.01, 1.97, 1.94

--- test_npc/vsrc/top.v
+++ test_npc/vsrc/top.v
@@ -7,31 +7,17 @@ module ysyx_24100005_top(
 
   output reg [31:0] PC
 );
-  wire [31:0] SPC;
-  wire [31:0] DPC;
-
-  wire wen; // reg write
   wire vaild; // mem read
-  wire write_mem; // mem write
-  wire [31:0] wdata; // reg write
-  wire [31:0] rs1data; // reg read rs1
-  wire [31:0] rs2data; // reg read rs2
 
   // for mem read extract and sext
   reg [31:0] mem_rdata;
 
   wire [6:0] opcode;
-  wire [2:0] funct3;
-  wire [4:0] rs1;
-  wire [4:0] rs2;
-  wire [4:0] rd;
-
 
   // adder input output
   wire [31:0] addr;
   wire [31:0] add_input1;
   wire [31:0] add_input2;
-
 
   // assign inst = 32'h0;
   assign add_input1 = 32'h0;
