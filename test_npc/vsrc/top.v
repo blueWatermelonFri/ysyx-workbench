d73ed94ca2ad07ed26a4f901bb8c1852f0a2eb01
tracer-ysyx <tracer@ysyx.org>
2024-11-28 11:03:07 +0800

    > sim RTL ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:03:07 up 69 days,  1:20,  0 user,  load average: 1.67, 1.96, 1.94

--- test_npc/vsrc/top.v
+++ test_npc/vsrc/top.v
@@ -1,22 +1,12 @@
-import "DPI-C" function int npcmem_read(input int raddr);
 
 module ysyx_24100005_top(
   input [31:0] inst,
   input rst,
   input clk
 );
-  wire vaild; // mem read
-  reg [31:0] mem_rdata;
-  wire [6:0] opcode;
-
-  // adder input output
-  wire [31:0] addr;
-  wire [31:0] add_input1;
-  wire [31:0] add_input2;
 
   always @(*) begin
     $display("inst = %h\n", rst);
-    
   end
 
 endmodule
