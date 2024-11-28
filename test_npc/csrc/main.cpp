0bcd9ccf799ea6b9365143f8097202450aadcfb2
tracer-ysyx <tracer@ysyx.org>
2024-11-28 10:27:03 +0800

    > sim RTL ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:27:03 up 69 days, 44 min,  0 user,  load average: 1.87, 1.94, 1.93

---
+++ test_npc/csrc/main.cpp
@@ -0,0 +1,60 @@
+#include "Vysyx_24100005_top___024root.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include <Vysyx_24100005_top.h>
+#include <stdio.h>
+#include <string.h>
+
+VerilatedContext* contextp = new VerilatedContext;
+VerilatedVcdC* tfp = NULL;
+static TOP_NAME top;
+
+extern "C" int npcmem_read(int raddr) {
+  uint32_t aligned_addr = raddr & (~0x3u);
+  return 1;
+}
+
+extern "C" void npcmem_write(int waddr, int wdata, char wmask) {
+  // 总是往地址为`waddr & ~0x3u`的4字节按写掩码`wmask`写入`wdata`
+  // `wmask`中每比特表示`wdata`中1个字节的掩码,
+  // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
+  uint32_t aligned_addr = waddr & (~0x3u);
+}
+
+void single_cycle() {
+
+  contextp->timeInc(1);
+  top.clk = 0; top.eval();
+  tfp->dump(contextp->time());
+
+  contextp->timeInc(1);
+  top.clk = 1; top.eval();
+  tfp->dump(contextp->time());
+
+}
+
+
+void reset(int n) {
+
+  top.rst = 1;
+  while (n -- > 0) single_cycle();
+  top.rst = 0;
+
+}
+
+int main(int argc, char *argv[]) {
+  // nvboard_bind_all_pins(&top);
+  // nvboard_init();
+
+  contextp->traceEverOn(true);
+  tfp = new VerilatedVcdC;
+  top.trace(tfp, 0);
+  tfp->open("/home/myuser/ysyx/ysyx-workbench/npc/simx.vcd");
+  
+
+  reset(3);
+
+  tfp->close();
+
+
+}
---
+++ test_npc/vsrc/top.v
@@ -0,0 +1,83 @@
+import "DPI-C" function int npcmem_read(input int raddr);
+
+module ysyx_24100005_top(
+  input [31:0] inst,
+  input rst,
+  input clk,
+
+  output reg [31:0] PC
+);
+  wire [31:0] SPC;
+  wire [31:0] DPC;
+
+  wire wen; // reg write
+  wire vaild; // mem read
+  wire write_mem; // mem write
+  wire [31:0] wdata; // reg write
+  wire [31:0] rs1data; // reg read rs1
+  wire [31:0] rs2data; // reg read rs2
+
+  // for mem read extract and sext
+  reg [31:0] mem_rdata;
+
+  wire [6:0] opcode;
+  wire [2:0] funct3;
+  wire [4:0] rs1;
+  wire [4:0] rs2;
+  wire [4:0] rd;
+
+
+  // adder input output
+  wire [31:0] addr;
+  wire [31:0] add_input1;
+  wire [31:0] add_input2;
+
+
+  // PC更新
+  ysyx_24100005_Reg #(32, 32'h8000_0000) i0 (.clk(clk),
+                                              .rst(rst), 
+                                              .din(DPC), 
+                                              .dout(PC), 
+                                              .wen(1'b1));
+  // static next pc
+  assign SPC = PC + 32'h0000_0004;
+
+  assign rs1 = inst[19:15];
+  assign rs2 = inst[24:20];
+  assign rd = inst[11:7];
+  assign funct3 = inst[14:12];
+  assign wdata = 0;
+
+
+  assign DPC = 32'h8000_0000;
+
+  // assign inst = 32'h0;
+  assign add_input1 = 32'h0;
+  assign add_input2 = 32'h0;
+  assign addr = add_input1 + add_input2;
+  assign opcode = inst[6:0];
+
+  // 判断指令是否有读写需求
+  ysyx_24100005_MuxKeyWithDefault #(1, 7, 1) Mux_read_mem (.out(vaild), 
+                                                              .key(opcode), 
+                                                              .default_out(1'b0), 
+                                                              .lut({
+                                                                    7'b000_0011,  1'b1  // load
+                                                                    }));
+  // assign vaild = 1;
+  // assign addr = 0;
+
+  always @(*) begin
+    $display("vaild = %h\n", vaild);
+    if (vaild) begin // 有读写请求时 
+      mem_rdata = npcmem_read(0);
+    end
+    else begin
+      mem_rdata = 0;
+    end
+  end
+
+
+
+endmodule
+
