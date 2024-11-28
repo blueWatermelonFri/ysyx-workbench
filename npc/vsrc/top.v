5dd8aa56585ccf9ad682376a614e8ddb1e559e60
tracer-ysyx <tracer@ysyx.org>
2024-11-28 22:40:55 +0800

    > sim RTL ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:40:55 up 69 days, 12:58,  0 user,  load average: 0.77, 0.82, 0.73

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -284,8 +284,8 @@ module ysyx_24100005_top(
 // 3. 尝试一个最小的可复现demo
   // assign wmask = 8'b00000001;
   // memory access
-  always @(rs1data) begin
-    $display("rs1data = %h, ", rs1data);
+  always @(wen) begin
+    $display("rs1data = %h, ", wen);
 
     // $display("%h %h %h %h %h, ", read_mem, write_mem, add_output, rs2data, wmask);
 
