742c7adf61314757c7d5f5864caaa230b225cd60
tracer-ysyx <tracer@ysyx.org>
2024-11-30 19:57:07 +0800

    > sim RTL ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:57:07 up 71 days, 10:14,  0 user,  load average: 0.70, 0.70, 0.72

--- npc/csrc/tool.cpp
+++ npc/csrc/tool.cpp
@@ -117,6 +117,7 @@ extern "C" void ebreak() {
 }
 
 extern "C" int npcmem_read(int raddr) {
+  printf("aligned_addr = %x\n", raddr);
   uint32_t aligned_addr = raddr & (~0x3u);
   return pmem_read(aligned_addr);
 }
