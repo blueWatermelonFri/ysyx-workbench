406238d0348b60eefdc196eb9336f8fcbe5e5d55
tracer-ysyx <tracer@ysyx.org>
2024-12-01 15:35:47 +0800

    > sim RTL ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:35:47 up 72 days,  5:53,  0 user,  load average: 0.49, 0.72, 0.72

--- npc/csrc/tool.cpp
+++ npc/csrc/tool.cpp
@@ -159,10 +159,29 @@ void end_wave(){
 }
 void reset(int n) {
 
-  top.rst = 1;
-  while (n -- > 0) single_cycle();
-  top.rst = 0;
+  // top.rst = 1;
+  // while (n -- > 0) single_cycle();
+  // top.rst = 0;
 
+  top.rst = 1;
+  top.clk = 0;
+  top.eval();
+  contextp->timeInc(1);
+  tfp->dump(contextp->time());  
+
+  while(n -- ){
+    top.clk = 1;
+    top.eval();
+    contextp->timeInc(1);
+    tfp->dump(contextp->time());  
+    if(n==0){
+      top.rst = 0;
+    }
+    top.clk = 0;
+    top.eval();
+    contextp->timeInc(1);
+    tfp->dump(contextp->time());  
+  }
 }
 
 void npc_execute_once(){
