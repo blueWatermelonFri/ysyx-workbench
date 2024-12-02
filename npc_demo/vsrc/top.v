dcd4b5c7a00a97a1df6cfabeaf8ac4e2d5ee20b1
tracer-ysyx <tracer@ysyx.org>
2024-12-02 17:30:31 +0800

    > sim RTL ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:30:31 up 73 days,  7:48,  0 user,  load average: 2.94, 2.36, 2.12

--- npc_demo/vsrc/top.v
+++ npc_demo/vsrc/top.v
@@ -9,12 +9,15 @@ module ysyx_24100005_top(
 );
   wire [31:0] SPC;
   wire [31:0] DPC;
-
   wire [31:0] rs1data; // reg read rs1
   wire [31:0] rs2data; // reg read rs2
-
   wire [6:0] opcode;
 
+  // 初始化寄存器堆
+  always @(posedge clk) begin
+    if (rst) PC <= 32'h0;
+    else PC <= SPC;
+  end
 
   // 初始化寄存器堆
   ysyx_24100005_RegisterFile #(5, 32) RegFile(
