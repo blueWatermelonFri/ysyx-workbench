25b0c5b792130f27cfed5e88c102011901d1fa73
tracer-ysyx <tracer@ysyx.org>
2024-12-03 20:59:10 +0800

    > sim RTL ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:59:10 up 74 days, 11:16,  0 user,  load average: 2.08, 1.96, 1.95

--- npc_demo/vsrc/top.v
+++ npc_demo/vsrc/top.v
@@ -128,12 +128,13 @@ module ysyx_24100005_top(
 
   // write back 
   // mux for weather write back 
-  ysyx_24100005_MuxKeyWithDefault #(2, 7, 1) Mux_write_reg (.out(wen), 
+  ysyx_24100005_MuxKeyWithDefault #(3, 7, 1) Mux_write_reg (.out(wen), 
                                                         .key(opcode), 
                                                         .default_out(1'b0), 
                                                         .lut({
                                                               7'b110_0011, 1'b0, // B type
-                                                              7'b010_0011, 1'b0  // store
+                                                              7'b010_0011, 1'b0,  // store
+                                                              7'b001_0011, 1'b1
                                                               }));
 
   // mux for update PC
