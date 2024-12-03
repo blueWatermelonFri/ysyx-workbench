d4aeb9f98086503cd2f369eb2605188de1d85de3
tracer-ysyx <tracer@ysyx.org>
2024-12-03 22:41:25 +0800

    > sim RTL ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:41:25 up 74 days, 12:59,  0 user,  load average: 2.66, 2.11, 2.09

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -286,8 +286,8 @@ module ysyx_24100005_top(
   // 为什么add_output变化会触发两次，因为第一次触发是下降沿rs1addr变了，
   // 第二次触发时上升沿rs1addr变了，所以add_output会变化两次
 
-  always @(PC) begin
-    if( PC != 0) begin
+  always @(PC, clk) begin
+    if( PC != 0 && !clk) begin
       inst = npcmem_read(PC);
     end
     else begin
