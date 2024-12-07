0d5c4b172550b4abac0f4a450ad7fdb562fa1de2
tracer-ysyx <tracer@ysyx.org>
2024-12-07 15:43:30 +0800

    > sim RTL ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:43:30 up 78 days,  6:01,  0 user,  load average: 1.55, 1.62, 1.60

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -301,10 +301,11 @@ module ysyx_24100005_top(
   assign is_ltu = carry == 0 ;
   assign is_gtu = carry == 1 ; //严格大于，不包括等于
 
-  always@(is_jump, is_equal, zero)begin
+  always@(is_jump, is_equal, zero, jump_data)begin
     $display("is jump = %h\n", is_jump);
     $display("is_equal = %h\n", is_equal);
     $display("zero = %h\n", zero);
+    $display("jump_data = %h\n", jump_data);
   end
   // mux for whether jump
   // slt/slti的判断逻辑是一样的，所以共用一套条件，这也是为什么slt和slt的funct3是相同的，sltu/sltiu同理
