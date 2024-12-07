487240dbafd8a3e933e2254abd44d7d64af73fb0
tracer-ysyx <tracer@ysyx.org>
2024-12-07 16:38:51 +0800

    > sim RTL ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:38:51 up 78 days,  6:56,  0 user,  load average: 2.03, 1.68, 1.62

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -474,7 +474,7 @@ module ysyx_24100005_top(
 
   always @(read_mem, adder_output, write_mem, rs2data, wmask) begin
 
-    $display("mem rdata = %h", read_mem);
+    $display("read_mem = %h", read_mem);
     $display("mem rdata = %h", mem_rdata);
     if (read_mem ) begin // 
       mem_rdata = npcmem_read(adder_output);
