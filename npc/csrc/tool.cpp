a63c6ce78e99230fbecaca80273909bb17fa385d
tracer-ysyx <tracer@ysyx.org>
2024-12-07 15:42:59 +0800

    > sim RTL ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:42:59 up 78 days,  6:00,  0 user,  load average: 1.54, 1.63, 1.61

--- npc/csrc/tool.cpp
+++ npc/csrc/tool.cpp
@@ -101,13 +101,13 @@ static inline void host_write(void *addr, int wdata, char wmask) {
 }
 
 static uint32_t pmem_read(uint32_t addr) {
-  printf("read addr %x\n", addr);
+  // printf("read addr %x\n", addr);
   uint32_t ret = host_read(guest_to_host(addr));
   return ret;
 }
 
 static void pmem_write(uint32_t addr, int wdata, char wmask) {
-  printf("write addr %x\n", addr);
+  // printf("write addr %x\n", addr);
   host_write(guest_to_host(addr), wdata, wmask);
 
 }
