e647e5377bc3206ce9768b5edd6f4a9cf596f6ef
tracer-ysyx <tracer@ysyx.org>
2024-12-08 14:16:49 +0800

    > sim RTL ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:16:49 up 79 days,  4:34,  0 user,  load average: 1.69, 1.55, 1.56

--- npc/csrc/tool.cpp
+++ npc/csrc/tool.cpp
@@ -166,8 +166,7 @@ extern "C" void npcmem_write(int waddr, int wdata, char wmask, int PC) {
   // 总是往地址为`waddr & ~0x3u`的4字节按写掩码`wmask`写入`wdata`
   // `wmask`中每比特表示`wdata`中1个字节的掩码,
   // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
-  printf("write_addr = %x\n", waddr);
-  printf("cur pc  = %x\n", PC);
+  printf("Info cur pc = %x write_addr = %x\n",  PC, waddr);
   uint32_t aligned_addr = waddr & (~0x3u);
   // printf("write addr = %x, data = %x, wmask = %x\n", aligned_addr, wdata, wmask);
 
