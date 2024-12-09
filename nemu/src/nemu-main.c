9cb1158f7ade031ccb63ae814bcf5a7bccc1d59f
tracer-ysyx <tracer@ysyx.org>
2024-12-09 22:17:04 +0800

    > compile NEMU ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:17:04 up 80 days, 12:34,  0 user,  load average: 2.12, 1.73, 1.61

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -23,6 +23,12 @@ int is_exit_status_bad();
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
 #ifdef CONFIG_TARGET_AM
+  printf("xxxxxxxxxxxxxxxx\n");
+  printf("xxxxxxxxxxxxxxxx\n");
+  printf("xxxxxxxxxxxxxxxx\n");
+  printf("xxxxxxxxxxxxxxxx\n");
+  printf("xxxxxxxxxxxxxxxx\n");
+  printf("xxxxxxxxxxxxxxxx\n");
   am_init_monitor();
 #else
   init_monitor(argc, argv);
