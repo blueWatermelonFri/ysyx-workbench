e48928582bd3a6a620f98e512e4e016d52a4307d
tracer-ysyx <tracer@ysyx.org>
2024-12-15 14:55:02 +0800

    > compile NEMU ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:55:02 up 86 days,  5:12,  0 user,  load average: 1.31, 1.48, 1.46

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,96 +5,107 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-      va_list ap;
-     char *s;
-     char d_s[64];
-     int d;
-     va_start(ap, fmt);
-     int j = 0;
-     while (*fmt){
-         if(*(fmt++) == '%'){
-           switch (*fmt++) {
-             case 's':              /* string */
-                 s = va_arg(ap, char *);
-                 for(size_t k=0; s[k]; k++){
-                   putch(s[k]);
-                   j++;
-                 }
-                 break;
-             case 'd':              /* int */
-                 d = va_arg(ap, int);
-                 s = itoa(d, d_s);
-                 for(size_t k=0; s[k]; k++){
-                   putch(s[k]);
-                   j++;
-                 }
-                 break;
-              case '%':              /* escape % */
-                 putch('%');
-                 j++;
-                 break;
-              default:              /* %后面没有构成格式化字符串的话，打印% */
-                 putch('%');
-                 j++;
-                 break;
-           }
-         }
-         else{
-           putch(*(fmt-1));
-                 j++;
+static size_t index = 0;
 
-         }
-     }
+void get_stdarg_string(const char *fmt, va_list *ap, char *s){
+  int d;
+  char d2s[64];
+  char escape[2] = {'%', '\0'};
+  char ordinary[2] = {fmt[index], '\0'};
 
-    va_end(ap);
-    return j;
+  if((fmt[index++]) == '%'){
+    switch (fmt[index++]) {
+      case 's':              /* string */
+          s = va_arg(*ap, char *);
+          return;
+          break;
+      case 'd':              /* int */
+          d = va_arg(*ap, int);
+          s = itoa(d, d2s);
+          return;
+          break;
+      case '%':              /* escape % */
+          s = escape;
+          return;
+          break;
+      default:              /* %后面没有构成格式化字符串的话，打印% */
+          s = escape;
+          return;
+          break;
+    }
+  }
+  else{
+      s = ordinary;
+      return;
+  }
 }
+// int printf(const char *fmt, ...) {
+//     index = 0;
+//      va_list ap;
+//      char *s;
+//      char d_s[64];
+//      int d;
+//      va_start(ap, fmt);
+//      int j = 0;
+//      char flag = '0';
+//      while (*fmt){
+//          if(*(fmt++) == '%'){
+//            switch (*fmt++) {
+//              case 's':              /* string */
+//                  s = va_arg(ap, char *);
+//                  for(size_t k=0; s[k]; k++){
+//                    putch(s[k]);
+//                    j++;
+//                  }
+//                  break;
+//              case 'd':              /* int */
+//                  d = va_arg(ap, int);
+//                  s = itoa(d, d_s);
+//                  for(size_t k=0; s[k]; k++){
+//                    putch(s[k]);
+//                    j++;
+//                  }
+//                  break;
+//               case '%':              /* escape % */
+//                  putch('%');
+//                  j++;
+//                  break;
+//               default:              /* %后面没有构成格式化字符串的话，打印% */
+//                  putch('%');
+//                  j++;
+//                  break;
+//            }
+//          }
+//          else{
+//            putch(*(fmt-1));
+//                  j++;
+
+//          }
+//      }
+
+//     va_end(ap);
+//     return j;
+// }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-     va_list ap;
-     char *s;
-     char d_s[64];
-     int d;
-     va_start(ap, fmt);
-     int j = 0;
-     while (*fmt){
-         if(*(fmt++) == '%'){
-           switch (*fmt++) {
-             case 's':              /* string */
-                 s = va_arg(ap, char *);
-                 for(size_t k=0; s[k]; k++){
-                   out[j++] = s[k];
-                 }
-                 break;
-             case 'd':              /* int */
-                 d = va_arg(ap, int);
-                 s = itoa(d, d_s);
-                 for(size_t k=0; s[k]; k++){
-                   out[j++] = s[k];
-                 }
-                 break;
-              case '%':              /* escape % */
-                 out[j++] = '%';
-                 break;
-              default:              /* %后面没有构成格式化字符串的话，打印% */
-                 out[j++] = '%';
-                 break;
-           }
-         }
-         else{
-           out[j++] = *(fmt-1);
-         }
-     }
-
-     va_end(ap);
-    out[j] = '\0';
-    // return strlen(j);
-    return j;
+  index = 0;
+  char *s = NULL;
+  int j = 0;
+  va_list ap;
+  va_start(ap, fmt);
+  while (*fmt){
+    get_stdarg_string(fmt, &ap, s);
+    for(size_t k=0; s[k] != '\0'; k++){
+      out[j++] = s[k];
+    }
+  }
+  va_end(ap);
+  out[j] = '\0';
+  return j;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
