42e8a058c4ccf7f24a4d635c8af57e5cfe1edc13
tracer-ysyx <tracer@ysyx.org>
2024-12-18 21:54:02 +0800

    > compile NEMU ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:54:02 up 89 days, 12:11,  0 user,  load average: 0.78, 0.69, 0.77

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,5 +1,6 @@
 #include <am.h>
 #include <nemu.h>
+#include <stdio.h>
 
 
 static uint64_t boot_time = 0;
@@ -18,6 +19,7 @@ void __am_timer_init() {
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uptime->us = read_time();
+  printf("__am_timer_uptime = %d\n", uptime->us);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
