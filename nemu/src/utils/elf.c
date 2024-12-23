1edbf23adf53fdac2d5d059e389477c4527110a7
tracer-ysyx <tracer@ysyx.org>
2024-12-23 20:39:53 +0800

    > compile NEMU ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:39:53 up  6:25,  0 user,  load average: 5.71, 4.05, 3.75

--- nemu/src/utils/elf.c
+++ nemu/src/utils/elf.c
@@ -1,10 +1,10 @@
 #include <common.h>
 #include <elf.h>
 
-uint32_t ftrace_func_begin[1000];
-uint32_t ftrace_func_end[1000];
+uint32_t ftrace_func_begin[1500];
+uint32_t ftrace_func_end[1500];
 uint32_t ftrace_func_count = 0;
-char ftrace_func_name[1000][128];
+char ftrace_func_name[1500][128];
 
 void init_elf(const char *filename) {
     FILE *file = fopen(filename, "rb");
