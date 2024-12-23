e3d59955f8c76c82f9e16306bfa254efa28713e3
tracer-ysyx <tracer@ysyx.org>
2024-12-23 20:23:26 +0800

    > compile NEMU ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:23:26 up  6:08,  0 user,  load average: 3.74, 3.51, 3.23

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -130,6 +130,7 @@ void init_monitor(int argc, char *argv[]) {
   /* Initialize differential testing. */
   init_difftest(diff_so_file, img_size, difftest_port);
 
+  printf("xxxxxxxxxxxxxxx = \n");
   /* Initialize parse elf file. */
   init_elf(elf_file);
 
