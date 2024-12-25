435f12aa0baad2d08c207bf527a8d92448722b99
tracer-ysyx <tracer@ysyx.org>
2024-12-25 22:39:07 +0800

    > compile NEMU ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:39:07 up 2 days,  8:24,  0 user,  load average: 1.43, 2.35, 3.02

--- nemu/src/device/device.c
+++ nemu/src/device/device.c
@@ -56,6 +56,7 @@ void device_update() {
       case SDL_KEYUP: {
         uint8_t k = event.key.keysym.scancode;
         bool is_keydown = (event.key.type == SDL_KEYDOWN);
+        printf("is_keydown = %d\n", is_keydown);
         send_key(k, is_keydown);
         break;
       }
