500f39b1e7ff25523b098e61852ae1d00e3b61e8
tracer-ysyx <tracer@ysyx.org>
2024-12-25 22:18:29 +0800

    > compile NEMU ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:18:29 up 2 days,  8:03,  0 user,  load average: 2.23, 3.62, 3.56

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -3,7 +3,13 @@
 
 #define KEYDOWN_MASK 0x8000
 
+static uint32_t read_key() {
+  uint32_t key = *(volatile uint32_t *)(KBD_ADDR);
+  return key;
+}
+
+
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
+  kbd->keycode = read_key();
   kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
 }
