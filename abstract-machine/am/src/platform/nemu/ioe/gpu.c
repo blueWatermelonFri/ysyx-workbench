0ebb954d058e22693f10bc211de6319fa5a57679
tracer-ysyx <tracer@ysyx.org>
2024-12-26 22:35:28 +0800

    > compile NEMU ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:35:28 up 3 days,  8:20,  0 user,  load average: 0.27, 0.30, 0.28

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -11,7 +11,7 @@ void __am_gpu_init() {
   int w = (*(volatile uint32_t *)(VGACTL_ADDR) >> 16) & 0x0000ffff;  // 
 
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w * h; i ++) fb[i] = 255;
+  for (i = 0; i < w * h; i ++) fb[i] = 301;
   outl(SYNC_ADDR, 1);
 }
 
