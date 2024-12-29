aba149cf597787689b518723da4ab515c0b0aa71
tracer-ysyx <tracer@ysyx.org>
2024-12-29 19:21:17 +0800

    > compile NEMU ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:21:17 up 6 days,  5:06,  0 user,  load average: 0.13, 0.20, 0.22

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -138,12 +138,10 @@ static int decode_exec(Decode *s) {
   INSTPAT_END();
 
   R(0) = 0; // reset $zero to 0
-  // uint32_t temp = inst_fetch(&s->snpc, 4);
 
   return 0;
 }
 
-__attribute__((noinline))
 int isa_exec_once(Decode *s) {
   s->isa.inst.val = inst_fetch(&s->snpc, 4);
   return decode_exec(s);
