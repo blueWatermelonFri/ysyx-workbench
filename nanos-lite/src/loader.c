f5c1961f10eb08eee543c1c596ac0e20693f9836
tracer-ysyx <tracer@ysyx.org>
2025-01-15 22:33:26 +0800

    > compile NEMU ysyx_24100005 何源宏 Linux icode-hpc 5.15.0-101-generic #111-Ubuntu SMP Tue Mar 5 20:16:58 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:33:26 up 12 days,  9:08,  0 user,  load average: 1.50, 1.50, 1.57

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -13,9 +13,13 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   extern const char ramdisk_start[];
   Elf32_Ehdr elf_header;
   Elf32_Phdr program_header;
+  uint32_t segOffset, segVirtAddr, segFileSize, segMemSize;
 
   uint8_t * ph_addr;
   memcpy(&elf_header, ramdisk_start, sizeof(Elf32_Ehdr));
+  // sizeof是个编译器的运算符？不是stdlib这些库的实现？
+  printf("%x\n", sizeof(Elf32_Ehdr));
+
   // assert(*(uint32_t *)elf->e_ident == 0x7F454C46);
   if (elf_header.e_ident[EI_MAG0] != ELFMAG0 ||
       elf_header.e_ident[EI_MAG1] != ELFMAG1 ||
@@ -24,14 +28,25 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
       printf("Error: Not a ELF format file!\n");
       assert(0);
   }
+  // e_phoff表示program header table file 在elf文件中的偏移
   ph_addr = (uint8_t *) (elf_header.e_phoff + ramdisk_start);
+
+  // e_phnum表示program header table file里面有几个entry
+  // phentsize表示program header table file中每个entry的大小
+  // p_type表示这个entry对应的section的类型
 
   for(int i = 0; i<elf_header.e_phnum; i++){
     memcpy(&program_header, ph_addr + elf_header.e_phentsize * i, sizeof(Elf32_Phdr));
-    printf("%x\n", program_header.p_type);
+    if(program_header.p_type == PT_LOAD){
+      segOffset = program_header.p_offset;
+      segFileSize = program_header.p_filesz;
+      segMemSize =  program_header.p_memsz;
+      segVirtAddr =  program_header.p_vaddr;
+      // MemSize >= FileSize 通常是因为bss段，https://stackoverflow.com/questions/27958743/difference-between-p-filesz-and-p-memsz-of-elf32-phdr
+      memcpy((uint8_t *) segVirtAddr, (uint8_t *) (segOffset + ramdisk_start), segFileSize);
+      memset((uint8_t *) (segVirtAddr + segFileSize), segMemSize-segFileSize, 0);
+    }
   }
-  printf("%d\n", elf_header.e_phnum);
-  printf("%d\n", elf_header.e_phentsize);
 
 
   return elf_header.e_ehsize;
