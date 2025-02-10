781917e214a21780a1c888680fe3f7b7d5888d9b
tracer-ysyx <tracer@ysyx.org>
2025-02-10 22:09:29 +0800

    > compile NEMU ysyx_24100005 何源宏 Linux bjdx 5.4.0-202-generic #222-Ubuntu SMP Fri Nov 8 14:45:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:09:29 up 25 days,  6:01,  0 user,  load average: 0.33, 0.35, 0.46

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,22 +1,23 @@
 #include <common.h>
 #include "syscall.h"
 
-void strace(int a0, int a1, int a2, int a3 ){
+void strace(int a0, int a1, int a2, int a3, int return_value ){
     switch (a0) {
     case 0:  Log("SYSCALL name : SYS_exit, ARG0 is %d,  ARG1 is %d, ARG2 is %d, no return", a1, a2, a3);  break;
-    case 1:  Log("SYSCALL name : SYS_yield, ARG0 is %d,  ARG1 is %d, ARG2 is %d, Return is %d", a1, a2, a3, 0);  break;
+    case 1:  Log("SYSCALL name : SYS_yield, ARG0 is %d,  ARG1 is %d, ARG2 is %d, Return is %d", a1, a2, a3, return_value);  break;
+    case 4:  Log("SYSCALL name : SYS_write, ARG0 is %d,  ARG1 is %d, ARG2 is %d, Return is %d", a1, a2, a3, return_value);  break;
+    case 9:  Log("SYSCALL name : SYS_brk, ARG0 is %d,  ARG1 is %d, ARG2 is %d, Return is %d", a1, a2, a3, return_value);  break;
     default: panic("Unhandled syscall ID = %d", a0);
   }
 }
 
   // SYS_open, 2
   // SYS_read, 3
-  // SYS_write, 4
   // SYS_kill, 5
   // SYS_getpid, 6
-  // SYS_close,
-  // SYS_lseek,
-  // SYS_brk,
+  // SYS_close, 7
+  // SYS_lseek, 8
+  // SYS_brk, 9
   // SYS_fstat,
   // SYS_time,
   // SYS_signal,
@@ -42,6 +43,21 @@ int write_to_serial(int fd, void *buf, size_t count){
   return -1;
 }
 
+// int SYS_brk_call(int increment){
+
+//   const unsigned char *p1 = (const unsigned char *) buf;
+
+//   if(fd == 1 || fd == 2){
+//     for(int i = 0; i < count; i++){
+//       putch(p1[i]);
+//     }
+//     return count;
+//   }
+
+//   return -1;
+// }
+
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1; // a7
@@ -49,18 +65,19 @@ void do_syscall(Context *c) {
   a[2] = c->GPR3; // a1
   a[3] = c->GPR4; // a2
 
-#ifdef CONFIG_STRACE
-  strace(a[0], a[1], a[2], a[3]);
-#endif
-
   int return_value = 0;
 
   switch (a[0]) {
     case 0:  halt(a[1]);  break;
     case 1:  yield(); break;
     case 4:  return_value = write_to_serial(a[1], (void *)a[2], a[3]);  break;
+    // case 9:  return_value = write_to_serial(a[1]);  break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+
+// #ifdef CONFIG_STRACE
+  strace(a[0], a[1], a[2], a[3], return_value);
+// #endif
 
   c->GPRx = return_value;
 }
