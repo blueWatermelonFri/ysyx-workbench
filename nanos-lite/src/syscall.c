c46283a58f007fb0429ec3e9868e79d57e9ecc17
tracer-ysyx <tracer@ysyx.org>
2025-02-10 21:23:00 +0800

    > compile NEMU ysyx_24100005 何源宏 Linux bjdx 5.4.0-202-generic #222-Ubuntu SMP Fri Nov 8 14:45:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:23:00 up 25 days,  5:15,  0 user,  load average: 0.32, 0.34, 0.35

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,21 +1,19 @@
 #include <common.h>
 #include "syscall.h"
 
-void strace(int type, int a0, int a1, int a2){
-    switch (type) {
-    case 0:  Log("SYSCALL name : SYS_exit, ARG0 is %d,  ARG1 is %d, ARG2 is %d, no return", a0, a1, a2);  break;
-    case 1:  Log("SYSCALL name : SYS_yield, ARG0 is %d,  ARG1 is %d, ARG2 is %d, Return  is %d", a0, a1, a2, 0);  break;
-    default: panic("Unhandled syscall ID = %d", type);
+void strace(int a0, int a1, int a2, int a3 ){
+    switch (a0) {
+    case 0:  Log("SYSCALL name : SYS_exit, ARG0 is %d,  ARG1 is %d, ARG2 is %d, no return", a1, a2, a3);  break;
+    case 1:  Log("SYSCALL name : SYS_yield, ARG0 is %d,  ARG1 is %d, ARG2 is %d, Return is %d", a1, a2, a3, 0);  break;
+    default: panic("Unhandled syscall ID = %d", a0);
   }
 }
 
-  // ,
-  // ,
-  // SYS_open,
-  // SYS_read,
-  // SYS_write,
-  // SYS_kill,
-  // SYS_getpid,
+  // SYS_open, 2
+  // SYS_read, 3
+  // SYS_write, 4
+  // SYS_kill, 5
+  // SYS_getpid, 6
   // SYS_close,
   // SYS_lseek,
   // SYS_brk,
@@ -30,6 +28,20 @@ void strace(int type, int a0, int a1, int a2){
   // SYS_times,
   // SYS_gettimeofday
 
+int write_to_serial(int fd, void *buf, size_t count){
+
+  const unsigned char *p1 = (const unsigned char *) buf;
+
+  if(fd == 1 || fd == 2){
+    for(int i = 0; i < count; i++){
+      putch(p1[i]);
+    }
+    return count;
+  }
+
+  return -1;
+}
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1; // a7
@@ -37,13 +49,18 @@ void do_syscall(Context *c) {
   a[2] = c->GPR3; // a1
   a[3] = c->GPR4; // a2
 
-// #ifdef CONFIG_STRACE
+#ifdef CONFIG_STRACE
   strace(a[0], a[1], a[2], a[3]);
-// #endif
+#endif
+
+  int return_value = 0;
 
   switch (a[0]) {
     case 0:  halt(a[1]);  break;
-    case 1:  yield(); c->GPRx = 0; break;
+    case 1:  yield(); break;
+    case 4:  return_value = write_to_serial(a[1], (void *)a[2], a[3]);  break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+
+  c->GPRx = return_value;
 }
