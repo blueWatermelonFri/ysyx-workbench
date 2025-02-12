e4235b9ca18eeb9e993e17bbf835493e98287bfc
tracer-ysyx <tracer@ysyx.org>
2025-02-12 22:03:12 +0800

    > compile NEMU ysyx_24100005 何源宏 Linux bjdx 5.4.0-202-generic #222-Ubuntu SMP Fri Nov 8 14:45:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:03:12 up 1 day,  7:11,  0 user,  load average: 0.45, 0.31, 0.34

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -3,7 +3,7 @@
 
 int main() {
   write(1, "first!\n", 100);
-  printf("Hello World from Navy-apps for the th time!\n");
+  printf("xxxxxxxHello World from Navy-apps for the th time!\n");
   // write(1, "Hello World!\n", 13);
   // int i = 2;
   // volatile int j = 0;
