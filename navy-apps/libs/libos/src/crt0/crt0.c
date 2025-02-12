3d2b89d7cc21f27245b68809a45231dbeaf2c61a
tracer-ysyx <tracer@ysyx.org>
2025-02-12 22:19:14 +0800

    > compile NEMU ysyx_24100005 何源宏 Linux bjdx 5.4.0-202-generic #222-Ubuntu SMP Fri Nov 8 14:45:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:19:14 up 1 day,  7:27,  0 user,  load average: 0.40, 0.42, 0.36

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -7,6 +7,8 @@ extern char **environ;
 void call_main(uintptr_t *args) {
   char *empty[] =  {NULL };
   environ = empty;
-  exit(main(0, empty, empty));
+  int ret_code = main(0, empty, empty);
+  printf("ret code  = %d\n");
+  exit(ret_code);
   assert(0);
 }
