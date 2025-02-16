5c9500641986a3603eaf3b7072d38aca9d0769c0
tracer-ysyx <tracer@ysyx.org>
2025-02-16 14:03:36 +0800

    > compile NEMU ysyx_24100005 何源宏 Linux bjdx 5.4.0-202-generic #222-Ubuntu SMP Fri Nov 8 14:45:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:03:36 up 2 days, 21:22,  0 user,  load average: 0.20, 0.24, 0.24

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,5 +1,17 @@
 #include <common.h>
 #include "syscall.h"
+
+void strace(int a0, int a1, int a2, int a3, int return_value ){
+  switch (a0) {
+  case 0:  Log("SYSCALL name : SYS_exit, ARG0 is %d,  ARG1 is %d, ARG2 is %d, no return", a1, a2, a3);  break;
+  case 1:  Log("SYSCALL name : SYS_yield, ARG0 is %d,  ARG1 is %d, ARG2 is %d, Return is %d", a1, a2, a3, return_value);  break;
+  case 4:  Log("SYSCALL name : SYS_write, fd is %d,  buf addr is %x, count is %d, Return is %d", a1, a2, a3, return_value);  break;
+  case 9:  Log("SYSCALL name : SYS_brk, new pb is %x,  ARG1 is %x, ARG2 is %d, Return is %x", a1, a2, a3, return_value);  break;
+  default: panic("Unhandled syscall ID = %d", a0);
+}
+}
+
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1; // a7
@@ -7,9 +19,18 @@ void do_syscall(Context *c) {
   a[2] = c->GPR3; // a1
   a[3] = c->GPR4; // a2
 
+  intptr_t return_value = 0;
+
+
   switch (a[0]) {
     case 0:  halt(a[1]);  break;
     case 1:  yield(); break;    
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+
+// #ifdef CONFIG_STRACE
+strace(a[0], a[1], a[2], a[3], return_value);
+// #endif
+
+
 }
