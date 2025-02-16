14b4b6e088188855a0a4848a6c6503503838ba30
tracer-ysyx <tracer@ysyx.org>
2025-02-16 15:13:27 +0800

    > compile NEMU ysyx_24100005 何源宏 Linux bjdx 5.4.0-202-generic #222-Ubuntu SMP Fri Nov 8 14:45:04 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:13:27 up 2 days, 22:31,  0 user,  load average: 0.13, 0.16, 0.17

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -70,7 +70,7 @@ int _write(int fd, void *buf, size_t count) {
 }
 
 extern char end;
-intptr_t program_break = (intptr_t)&end;
+static intptr_t program_break = (intptr_t)&end;
 
 void *_sbrk(intptr_t increment) {
 
